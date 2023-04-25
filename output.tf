67c21a479548a095625879997dbf225e1d4f98b4
Chris Edillon <jce@redhat.com>
2023-04-24 21:30:35 -0400

    updated outputs

--- output.tf
+++ output.tf
@@ -6,10 +6,6 @@ output "bastion_public_fqdn" {
   description = "Public FQDN of the AAP bastion"
   value       = aws_instance.bastion.public_dns
 }
-output "bastion_public_ip" {
-  description = "Public IP address of the AAP bastion"
-  value       = aws_instance.bastion.public_ip
-}
 output "bastion_private_fqdn" {
   description = "Private FQDN of the AAP bastion"
   value       = aws_instance.bastion.private_dns
@@ -18,10 +14,6 @@ output "bastion_private_fqdn" {
 output "controller_public_fqdns" {
   description = "Public FQDN of the AAP controllers"
   value       = aws_instance.controller.*.public_dns
-}
-output "controller_public_ips" {
-  description = "Public IP address of the AAP controllers"
-  value       = aws_instance.controller.*.public_ip
 }
 output "controller_private_fqdns" {
   description = "Private FQDN of the AAP controllers"
@@ -32,21 +24,25 @@ output "hub_public_fqdns" {
   description = "Public FQDN of the AAP hubs"
   value       = aws_instance.hub.*.public_dns
 }
-output "hub_public_ips" {
-  description = "Public IP address of the AAP hubs"
-  value       = aws_instance.hub.*.public_ip
-}
 output "hub_private_fqdns" {
   description = "Private FQDN of the AAP hubs"
   value       = aws_instance.hub.*.private_dns
 }
 
+output "database_public_fqdns" {
+  description = "Public FQDN of the AAP databases"
+  value       = aws_instance.database.*.public_dns
+}
 output "database_private_fqdns" {
-  description = "Private FQDN of the AAP hubs"
+  description = "Private FQDN of the AAP databases"
   value       = aws_instance.database.*.private_dns
 }
 
+output "execution_public_fqdns" {
+  description = "Public FQDN of the AAP execution nodes"
+  value       = aws_instance.execution.*.public_dns
+}
 output "execution_private_fqdns" {
-  description = "Private FQDN of the AAP hubs"
+  description = "Private FQDN of the AAP execution nodes"
   value       = aws_instance.execution.*.private_dns
 }
